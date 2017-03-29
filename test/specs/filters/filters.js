11f143b19886b0ff33e992ad8850a3142e6fd472
heyhey <fruitsensatie@live.nl>
2017-03-29 13:32:26 +0200

    Updated filter testen

--- test/specs/filters/filters.js
+++ test/specs/filters/filters.js
@@ -11,8 +11,22 @@ describe("Filters: hoofdletterFilter en boldFilter", function() {
 	});
 
 	it("should make the first letter capitalized", function() {
-		var input = "hAllo";
+		var input = "hallo";
 		var output = "Hallo";
+
+		expect(filter(input)).toBe(output);
+	});
+
+	it("should make the first letter capitalized, and the second not", function() {
+		var input = "sChool";
+		var output = "School";
+
+		expect(filter(input)).toBe(output);
+	});
+
+	it("should make still work, and not throw an error if the letter is capitalized", function() {
+		var input = "Angular";
+		var output = "Angular";
 
 		expect(filter(input)).toBe(output);
 	});
